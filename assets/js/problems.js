83871305e47f9aa39899f0061e3680d667b7002b
Ian <61957594+Igyeom@users.noreply.github.com>
2026-07-19 07:06:25 +0900

    Add files via upload

---
+++ assets/js/problems.js
@@ -0,0 +1,14 @@
+// temporary problems until caden sends me the actual problems when he wakes up lol
+
+let problems = [
+	{
+		"title": "Hello.",
+		"description": "Hello.",
+		"latex": String.raw`hello.`
+	},
+	{
+		"title": "Hi.",
+		"description": "Hi.",
+		"latex": String.raw`hi.`
+	}
+];
